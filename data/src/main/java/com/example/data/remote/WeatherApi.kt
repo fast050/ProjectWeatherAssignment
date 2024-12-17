a6daa913b465dd68f2f509e3234cc0769ec5569a
Khalid <dragonman050@gmail.com>
2024-12-17 07:25:49 +0400

    - Secure API Key to make request for weather api

--- data/src/main/java/com/example/data/remote/WeatherApi.kt
+++ data/src/main/java/com/example/data/remote/WeatherApi.kt
@@ -4,25 +4,26 @@ import com.example.data.remote.dto.CountryInfoResponse
 import com.example.data.remote.dto.CurrentWeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
+import com.example.data.BuildConfig
+
 
 interface WeatherApi {
 
     @GET("search.json")
     suspend fun getCountries(
         @Query("q") query: String,
-        @Query("key") key: String = KEY,
+        @Query("key") key: String = BuildConfig.API_KEY,
     ) : List<CountryInfoResponse>
 
     @GET("current.json")
     suspend fun getCurrentWeather(
         @Query("q") query: String,
-        @Query("key") key: String = KEY,
+        @Query("key") key: String = BuildConfig.API_KEY,
     ) : CurrentWeatherResponse
 
 
     companion object {
         const val BASE_URL = " http://api.weatherapi.com/v1/"
-        private const val KEY = "2267eb08d63c4ae49c5174928241412"
     }
 
 
